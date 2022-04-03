7beb109d90bce8f25736367676b1d0abd1a375d7
ViphuKN <viphu12@gmail.com>
2022-04-02 23:42:37 -0400

    Progress into making the map filter out the markers

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -8,7 +8,7 @@ function getRandomIntInclusive(min, max) {
 }
 
 function resotArrayMake(dataArray) {
-  console.log('fired dataHandler');
+  // console.log('fired dataHandler');
   // console.table(dataArray); // this is called "dot notation"
   const range = [...Array(15).keys()];
   const listItems = range.map((item, index) => {
@@ -23,7 +23,7 @@ function resotArrayMake(dataArray) {
 // Injection function
 function createHtmlList(collection) {
   // console.log('fired HTML creator');
-  console.log('createHtml', collection);
+  // console.log('createHtml', collection);
   const targetList = document.querySelector('.resto-list');
   targetList.innerHTML = '';
   collection.forEach((item) => {
@@ -31,6 +31,29 @@ function createHtmlList(collection) {
     const displayName = name.toLowerCase();
     const injectThisItem = `<li>${displayName}</li> ${item.zip}<br></li>`;
     targetList.innerHTML += injectThisItem;
+  });
+}
+
+// Map function
+function initMap(targetId) {
+  const latLong = [38.784, -76.872];
+  const map = L.map(targetId).setView(latLong, 17);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+  }).addTo(map);
+  return map;
+}
+
+function addMapMarkers(map, collection) {
+  collection.forEach((item) => {
+    const point = item.geocoded_column_1?.coordinates;
+    console.log(item.geocoded_column_1?.coordinates);
+    L.marker([point[1], point[0]]).addTo(map);
   });
 }
 
@@ -42,14 +65,24 @@ async function mainEvent() { // the async keyword means we can make API requests
 
   const resto = document.querySelector('#resto_name');
   const zipcode = document.querySelector('#zipcode');
+  const map = initMap('map');
+  const retrievalVar = 'restaurants';
   submit.style.display = 'none'; // it's better not to display this until the data has loaded
 
-  const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
-  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-  // console.log(arrayFromJson);
+  if (localStorage.getItem(retrievalVar) === undefined) {
+    const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
+    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+    console.log(arrayFromJson);
+    localStorage.setItem(retrievalVar, JSON.stringify(arrayFromJson));
+  }
+
+  const storedDataString = localStorage.getItem(retrievalVar);
+  const storedDataArray = JSON.parse(storedDataString);
+  console.log(storedDataArray);
+  // const arrayFromJson = {data: []};
 
   // This if statement is to prevent a race condition on data load
-  if (arrayFromJson.length > 0) {
+  if (storedDataArray.length > 0) {
     submit.style.display = 'block';
     // this allows us to change the var to anything we want, but pre-sets it as an array for reasoning
     let currentArray = [];
@@ -67,6 +100,7 @@ async function mainEvent() { // the async keyword means we can make API requests
       createHtmlList(restaurants);
     });
 
+    // A filter for zipcode input
     zipcode.addEventListener('input', (event) => {
       if (!currentArray.length) { return; }
       console.log(event.target.value);
@@ -81,9 +115,10 @@ async function mainEvent() { // the async keyword means we can make API requests
       // console.log('form submission'); // this is substituting for a "breakpoint"
       // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
-      currentArray = resotArrayMake(arrayFromJson);
+      currentArray = resotArrayMake(storedDataArray);
       console.log(currentArray);
       createHtmlList(currentArray);
+      addMapMarkers(map, currentArray);
     });
   }
 }
